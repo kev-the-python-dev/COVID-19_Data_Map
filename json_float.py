911d10e211faabbe72d34d41656fe2371fc19947
Kevin Douglas <kevindouglas@Kevins-MacBook-Pro.local>
2022-04-27 18:08:58 -0700

    added script description to json_float

--- json_float.py
+++ json_float.py
@@ -1,9 +1,14 @@
 import json
 
+'''With our original we ran into an issue where the floating #'s were converted to string format. GeoJSON can not read this and map out location points (Long and Lat) without some kind of numerical value. This script fixes that issue. It is ran alongside the main script, so there is no need to touch/modify this.'''
+
 def conv_to_float():
+    
     file = 'geocovdata.json'
+    
     with open(file) as f:
         json_data = json.load(f)
+        
         for feature in json_data['features']:
             coordinates = feature['geometry']['coordinates']
             feature['geometry']['coordinates'] = [float(coord) for coord in coordinates]
